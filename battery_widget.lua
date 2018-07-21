978ad93202ec03c8f2914996f475f082b7c08c9d
Nightfeather <ss900328@gmail.com>
2018-07-21 15:47:56 +0800

    Add estimated battery time

--- battery_widget.lua
+++ battery_widget.lua
@@ -6,6 +6,16 @@ local naughty = require("naughty")
 local BatteryWidget = {}
 BatteryWidget.__index = BatteryWidget
 
+function min2hm(mins)
+  res = ""
+  mins = math.floor(mins/5)*5
+  if mins > 90 then
+    res = res .. string.format("%2d h ", math.floor(mins / 60))
+  end
+  res = res .. string.format("%3d m", mins % 60)
+  return res
+end
+
 function BatteryWidget:new( battery )
   local obj = {}
   setmetatable(obj, BatteryWidget)
@@ -14,6 +24,8 @@ function BatteryWidget:new( battery )
 
   obj.widget = wibox.widget.textbox()
   obj.tooltip = awful.tooltip({ objects = {obj.widget},})
+
+  obj.state = 0
 
   obj:update_widget()  
 
@@ -36,10 +48,13 @@ function BatteryWidget:update_widget()
   bat_stat = self:read_info("status")
   
   if bat_stat:find("Full") then
+    self.state = 2
     mark = "*"
   elseif bat_stat:find("Discharging") then
+    self.state = 0
     mark = "-"
   else
+    self.state = 1
     mark = "+"
   end
 
@@ -57,6 +72,7 @@ function BatteryWidget:update_tooltip()
   bat_voltage = tonumber(self:read_info("voltage_now")) / 1000000
   bat_design_full = tonumber(self:read_info("charge_full_design"))
   bat_full = tonumber(self:read_info("charge_full"))
+  bat_charge = tonumber(self:read_info("charge_now")) / 1000
 
   table.insert(a, string.format("Name:    %6s", self.battery_name))
   table.insert(a, string.format("Voltage: %2.2fV", bat_voltage))
@@ -65,6 +81,14 @@ function BatteryWidget:update_tooltip()
     table.insert(a, string.format("Current: %2.2fA", bat_current/1000))
   else
     table.insert(a, string.format("Current: %4.0fmA", bat_current))
+  end
+
+  if self.state == 0 then
+    table.insert(a, "Est.D.Time: " .. min2hm( (bat_charge / bat_current) * 60 ))
+  elseif self.state == 1 then
+    table.insert(a, "Est.C.Time: " .. min2hm( ( (bat_full/1000 - bat_charge) / bat_current) * 60 ))
+  elseif self.state == 2 then
+    table.insert(a, "Est.D.Time:  N/A")
   end
 
   table.insert(a, string.format("Health: %2.2f%%", (bat_full / bat_design_full) * 100))
